eb97283431efd60992d4ba6902d27c35a04af61a
Edward Guo <edward.h.guo@gmail.com>
2017-06-24 12:46:26 -0700

    testing webpack

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>hello world</div>
+    );
+  }
+}
+
+render((
+  <App />
+), document.getElementById('root'));
